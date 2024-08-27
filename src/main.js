e41fe255de2b6ab81154ef1d8ad440f44e166dd7
renjithram995 <rrenjith1995@gmail.com>
2024-08-27 20:53:57 +0530

    add bootstrap initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
 
 createApp(App).use(store).use(router).mount('#app')
